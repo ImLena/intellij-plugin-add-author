cb99b958f82ab4724cb4a0fffc0245056849d25b
GitHub Action <action@github.com>
2022-07-03 14:46:02 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/imlena/intellijpluginaddauthor/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.imlena.intellijpluginaddauthor.services
+
+import com.github.imlena.intellijpluginaddauthor.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/imlena/intellijpluginaddauthor/services/MyProjectService.kt
+++ src/main/kotlin/com/github/imlena/intellijpluginaddauthor/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.imlena.intellijpluginaddauthor.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.imlena.intellijpluginaddauthor.MyBundle
 
 class MyProjectService(project: Project) {
 
